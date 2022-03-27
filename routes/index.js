ae331d7f5f1f0cb63021e763046bf621d8ee0add
Trung DevOps NC <trung.nguyen@shopdi.co>
2022-03-27 04:27:01 +0000

    Change something to trigger CD

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Devops Team' });
+  res.render('index', { title: 'Devops Team from Shopdi' });
 });
 
 module.exports = router;
